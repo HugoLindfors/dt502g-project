7169204ba713d6c972f0265b94519dfcbc8b1350
Hugo Lindfors <HugoLindfors@users.noreply.github.com>
2025-10-23 12:00:34 +0200

    minor changes

--- main.py
+++ main.py
@@ -1,16 +1,31 @@
+# Imports
 from pygame import init, quit, QUIT
 from pygame.display import set_caption, set_mode
 from pygame.event import get
 
-init()
 
-screen = set_mode((400, 300))
-set_caption("Hello Pygame")
+def main():
+    # Init
+    init()
 
-loop_should_break = False
-while not loop_should_break:
-    for event in get():
-        if event.type == QUIT:
-            loop_should_break = True
+    # Screen settings
+    HEIGHT = 300
+    WIDTH = 400
+    CAPTION = "DT502G Project - The Game"
 
-quit()
+    screen = set_mode((WIDTH, HEIGHT))
+    set_caption(CAPTION)
+
+    # Game loop
+    loop_should_break = False
+    while not loop_should_break:
+        for event in get():
+            if event.type == QUIT:
+                loop_should_break = True
+
+    # Quit
+    quit()
+
+
+if __name__ == "__main__":
+    main()
