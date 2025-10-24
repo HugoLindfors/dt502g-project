c319c9825d3958704580ea412385084e46b51469
paolomarkos <paolomarkos06@gmail.com>
2025-10-24 11:02:53 +0200

    player movement update

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ pygame.display.set_caption("DT502G Project - The Game")
 # --- Load and Scale Player Image ---
 player_img = pygame.image.load('img/player.png').convert_alpha()
 
-# Resize image (change 80, 80 to any size you want)
+# Resize image
 player_img = pygame.transform.scale(player_img, (80, 80))
 player_width, player_height = player_img.get_size()
 
@@ -41,16 +41,16 @@ while not done:
 
         
     dx, dy = 0, 0
-    if keys[pygame.K_LEFT]:
+    if keys[pygame.K_a]:
         dx = -1
-    if keys[pygame.K_RIGHT]:
+    if keys[pygame.K_d]:
         dx = 1
-    if keys[pygame.K_UP]:
+    if keys[pygame.K_w]:
         dy = -1
-    if keys[pygame.K_DOWN]:
+    if keys[pygame.K_s]:
         dy = 1
 
-    if dx != 0 and dy != 0:
+    if dx != 0 and dy != 0:  #diagonal movement correction (would be too fast otherwise)
         dx *= 0.7071  # 1/sqrt(2)
         dy *= 0.7071
 
@@ -59,26 +59,18 @@ while not done:
     x = max(0, min(screen_width - player_width, x))
     y = max(0, min(screen_height - player_height, y))
     
-    screen.fill(WHITE)
+    screen.fill(GREEN)
     screen.blit(player_img, (x, y))
 
     
-    # Here, we clear the screen to white. Don't put other drawing commands
-    # above this, or they will be erased with this command.
-    
-    # If you want a background image, replace this clear with blit'ing the
-    # background image.
-  
-    
- 
+   
     # --- Drawing code should go here
     # pygame.draw.rect(screen,RED,(x,y,rect_width,rect_height)) #(förflyttning i x-led, förflyttning i y-led, bredd, höjd)
-    
-    # --- Go ahead and update the screen with what we've drawn.
+
+    # update the screen with what we've drawn.
     pygame.display.flip()
  
     # --- Limit to 60 frames per second
     clock.tick(60)
  
-# Close the window and quit.
 pygame.quit()
