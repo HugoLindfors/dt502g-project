fc1f8e3d31681e3525271710119b5e808be4ae13
gustavvising <gustav.vising@gmail.com>
2025-11-04 14:14:57 +0100

    Update enemy.py
    More clear variable names and comments

--- entities/enemies/enemy.py
+++ entities/enemies/enemy.py
@@ -2,29 +2,56 @@ from entities import Entity
 import threading
 import time
 
-class Enemy(Entity):
-    fire_delay = 3  # seconds between firing projectiles
-    max_x_leftright_move = 10  # max horizontal move distance
+"""enemy.py: A template for creating enemy entities."""
 
-    def __init__(self, health: int, x: int, y: int):
+__author__ = "Gustav Vising"
+
+class Enemy(Entity):
+
+    tick = 1 / 60 # Sleep delay for updating the enemy
+
+    # Variables that will be initialized 
+    enemy_health = -1
+    enemy_spawn_position_x = -1
+    enemy_spawn_position_y = -1
+    enemy_maximum_allowed_right = -1
+    enemy_maximum_allowed_left = -1
+
+    # Constants
+    enemy_delay_between_fire = 3  # seconds between firing projectiles
+    enemy_maximum_horizontal_movement = 10  # both left and right from the spawn position
+    enemy_y_moving_direction = -1 # the enemy moves downwards
+
+    # Changing variables
+    enemy_x_moving_direction = -1
+
+    def __init__(self, name: str, img_path: str, width: int, height: int, x: int, y: int, health: int):
         super().__init__()
 
-        # Basic attributes
-        self.health = health
-        self.spawn_x = x
-        self.spawn_y = y
-        self.maximum_allowed_right_x = x + self.max_x_leftright_move
-        self.minimum_allowed_left_x = x - self.max_x_leftright_move
+        # Entity class variables
+        self.name = name
+        self.img_path = img_path
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
 
-        # Movement and control
-        self.x_moving_direction = -1
-        self.y_moving_direction = 0
-        self.alive_event = threading.Event()  # controls lifetime
-        self.health_lock = threading.Lock()
+        # Enemy class variables
+        self.enemy_health = health
+        self.enemy_spawn_position_x = x
+        self.enemy_spawn_position_y = y
 
+        # Calculate values
+        self.enemy_maximum_allowed_right = x + self.max_x_leftright_move
+        self.enemy_maximum_allowed_left = x - self.max_x_leftright_move
+
+        # Spawn position
         self.setPosition(x, y)
 
-        # Start behavior threads
+        # Events
+        self.alive_event = threading.Event()
+        self.health_lock = threading.Lock()
+
         self.move_thread = threading.Thread(target=self.move, daemon=True)
         self.fire_thread = threading.Thread(target=self.fire, daemon=True)
         self.alive_thread = threading.Thread(target=self.alive, daemon=True)
@@ -35,39 +62,41 @@ class Enemy(Entity):
 
     def move(self):
         while not self.alive_event.is_set():
-            if self.x <= self.minimum_allowed_left_x:
-                self.x_moving_direction = 1
-            elif self.x >= self.maximum_allowed_right_x:
-                self.x_moving_direction = -1
+            if self.x <= self.enemy_maximum_allowed_left:
+                self.enemy_x_moving_direction = 1
+            elif self.x >= self.enemy_maximum_allowed_right:
+                self.enemy_x_moving_direction = -1
 
-            self.x += self.x_moving_direction
-            self.y += self.y_moving_direction
-            time.sleep(1 / 60)  # simulate frame rate
+            self.x += self.enemy_x_moving_direction
+            self.y += self.enemy_y_moving_direction
+            time.sleep(self.tick)
 
+    """ Lifetime of the entity """
     def alive(self):
         while not self.alive_event.is_set():
             with self.health_lock:
-                if self.health <= 0:
+                if self.enemy_health <= 0:
                     # Stop all activity
                     self.alive_event.set()
                     # TODO: turn the entity into a scrap
                     break
-            time.sleep(1 / 60)
+            time.sleep(self.tick)
 
+    """ The enemy fires projectiles to attack the player """
     def fire(self):
         while not self.alive_event.is_set():
             # TODO: Implement projectile firing
-            time.sleep(self.fire_delay)
+            time.sleep(self.enemy_delay_between_fire)
 
+    """Called when the enemy is hit by a projectile."""
     def hit(self):
-        """Called when the enemy is hit by a projectile."""
         with self.health_lock:
-            self.health -= 1
+            self.enemy_health -= 1
 
     def getHealth(self):
         with self.health_lock:
-            return self.health
+            return self.enemy_health
 
     def setHealth(self, health: int):
         with self.health_lock:
-            self.health = health
+            self.enemy_health = health
