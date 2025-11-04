d35e9761bc4ee9d896765ae41bcfefda1224293a
gustavvising <gustav.vising@gmail.com>
2025-11-04 15:01:32 +0100

    Update boss.py
    Declared the variables with self to access the variables in the entity class

--- entities/enemies/boss.py
+++ entities/enemies/boss.py
@@ -7,12 +7,13 @@ __author__ = "Gustav Vising"
 class Boss(Enemy):
 
     def __init__(self):
-        super().__init__()
 
-        name = "Boss"
-        img_path = "images/boss.jpg"
-        width = 100
-        height = 100
-        x = 50
-        y = 50
-        health = 30
+        self.name = "Boss"
+        self.img_path = "images/boss.jpg"
+        self.width = 100
+        self.height = 100
+        self.x = 50
+        self.y = 50
+        self.health = 30
+
+        super().__init__()
